f8afb8e0b7fce5d8364a25f1dc84c052306cb292
Sinenhlanhla Magubane <snenhlanhlamagubane99@gmail.com>
2025-09-25 10:29:47 +0200

    Created left navigation section for the webpage (desktop screens)

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar"
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -9,7 +9,27 @@ function App() {
         <div className="navbar">
           <Navbar />
         </div>
-        
+        <main>
+          {/* top section with search and profile sections */}
+          <div className="topNavBar">
+            <div className="search"></div>
+            <div className="topRightNav"></div>
+          </div>
+
+          {/* cards section for the weather layout */}
+
+          {/* card details according to each corresponding day */}
+          <div className="details">
+            <div className="cardDetails"></div>
+
+            {/* chances of rain data insights */}
+            <div className="chanceOfRain"></div>
+          </div>
+          <div className="mapAndCities">
+            <div className="globalMap"></div>
+            <div className="citiesNearby"></div>
+          </div>
+        </main>
       </div>
     </>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { TiWeatherPartlySunny } from "react-icons/ti";
 import { TiHomeOutline } from "react-icons/ti";
 import { FaLocationDot } from "react-icons/fa6";
@@ -10,17 +9,17 @@ import { IoSettings } from "react-icons/io5";
 export default function Navbar() {
   return (
     <div className="navSection">
-      <div className="logo">
+      <div className="logo" style={{cursor:'pointer'}}>
         <TiWeatherPartlySunny size={58} />
         <h3>Weather</h3>
       </div>
-      <div className="midNav">
+      <div className="midNav" style={{cursor:'pointer'}}>
         <TiHomeOutline size={58} />
         <FaLocationDot size={58} />
         <CiMap size={58} />
         <GrNotes size={58} />
       </div>
-      <div className="bottomNav">
+      <div className="bottomNav" style={{cursor:'pointer'}}>
         <FaUserAlt size={58} />
         <IoSettings size={58} />
       </div>
